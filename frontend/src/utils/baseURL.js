4aa79f294af0231c407b1f2c4c14f8548a169b68
rishitjindal <rishitjindal2@gmail.com>
2025-06-29 00:20:32 +0530

    Update baseURL.js

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,10 @@
+
+// src/utils/baseURL.js
+
 const getBaseUrl = () => {
-    return "http://localhost:5000"
-}
+  return import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+};
 
 export default getBaseUrl;
+
+
