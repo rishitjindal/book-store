d320144f5dfa74943b71f26323807634534386fb
rishitjindal <rishitjindal2@gmail.com>
2025-06-30 16:38:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+
 
 // middleware
 app.use(express.json());
